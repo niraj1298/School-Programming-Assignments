c9ff8c12bbe468d2759c5db950ca479a39ee6e93
Niraj Nepal <niraj.nepal70@gmail.com>
2022-09-05 22:50:29 -0400

    Update EasterDateCalculator.cpp

--- EasterDateCalculator.cpp
+++ EasterDateCalculator.cpp
@@ -3,46 +3,33 @@
  Date: 2022-08-28
  Program Name: When's Easter Day!
  Summary: This program will calculate the date of Easter Day for a given year.
- Credits: Notes from Program Description, Few resources from internet, Research on Golden Number.
- Inspired by https://www.daniweb.com/programming/software-development/threads/463261/c-easter-day-calculation ( Some variable naming are similar ).
+ Credits: Notes from Program Description
  **/
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int goldenNumber, year, a, b, c, d, e, f, g, h, i, k, l, m, p, EasterMonth, EasterDay;
-    bool leapYear;
-
+    int goldenNumber,y,c,x,d,e,n,z;
     cout << "Please Enter the year > ";
-    cin >> year;
+    cin >> y;
+    goldenNumber = (y % 19) + 1;
+    //
+    c = floor((y / 100) + 1);
+    x = floor(((3 * c) / 4) - 12);
+    z = floor(((8*c+ 5) / 25)-5);
+    d = floor(((5*y)/4)-x-10);
+    e = ((11*goldenNumber)+20+z-x)%30;
+    if(e == 25 && goldenNumber > 11 || e == 24){ e++; }
 
-    a = year % 19;                                              // calculates the Golden Number, which is a number used to determine the date of Easter.
-    b = year / 100;                                             // calculates the century of the year.
-    c = year % 100;                                             // calculates the year within the century.
-    d = b / 4;                                                  // calculates the number of years since the year was divided by 4.
-    e = b % 4;                                                  // calculates the number of years since the year was divided by 4.
-    f = (b + 8) / 25;                                           // calculates the number of years since the year was divided by 25.
-    g = (b - f + 1) / 3;                                        // calculates the number of years since the year was divided by 3.
-    h = (19 * a + b - d - g + 15) % 30;                         // calculates the number of days since the year was divided by 30.
-    i = c / 4;                                                  // calculates the number of years since the year was divided by 4.
-    k = c % 4;                                                  // calculates the number of years since the year was divided by 4.
-    l = (32 + 2 * e + 2 * i - h - k) % 7;                       // calculates the number of days since the year was divided by 7.
-    EasterMonth = (h + l - 7 * m + 114) / 31;                   // calculates the month of Easter.
-    p = (h + l - 7 * m + 114) % 31;                             // calculates the day of Easter.
-    EasterDay = p + 1;                                          // calculates the day of Easter.
 
-    if ((EasterMonth == 3) && (EasterDay > 31)) // The if statement is used to check if EasterMonth is equal to 3 and EasterDay is greater than 31.
-    {                                           // If both conditions are true, then EasterMonth is set to 4 and EasterDay is set to EasterDay - 31.
-        EasterMonth = 4;
-        EasterDay = EasterDay - 31;
-    }
+    n = 44 - e;
+    if (n < 21){ n = n + 30;} else{n = n;}
+    n = n+7-((d+n)%7);
+    if(n > 31){n = n-31; cout << "Easter Day is on April " << n <<"/" <<  y <<  endl;} else{cout << "Easter Day is on March " << n <<"/" << y << endl;}
 
-    cout << "Easter falls on " << EasterDay << "/" << EasterMonth << "/" << year << "." << endl;
 
     return 0;
 }
-
-
-
